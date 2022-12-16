2c44eb788cae0a6e999f916ec6233a308f29de72
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2022-12-16 20:54:33 +0600

    Update 3.2

--- main/app.py
+++ main/app.py
@@ -4,20 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 from time import sleep
 from threading import Thread
 
-__version__=3.0
+__version__=3.2
 
 amount_of_post_each_page=5
 
 page_ids=[
-    "100064620321323",
-    "100083712786515",
-    "100082319246979",
-    "100057273376837",
-    "100077463865033",
-    # "109057208214046",
-    # "110175880338880",
-    # "102260178961056",
-    "100047072903106",
+	"100064620321323",
+	"100083712786515",
+	"100082319246979",
+	"100057273376837",
+	"100077463865033",
+	# "109057208214046",
+	# "110175880338880",
+	# "102260178961056",
+	"100047072903106",
 ]
 
 acc_ie='datr=I-OXY2hRUMNU0E1dAZskV8QM; sb=I-OXY6nujoTuO6pyN4G_171u; m_pixel_ratio=1.84375; fr=0emoGyVJMegntNkin.AWUyCee8uv5hhCFFKlbQ12LyFXE.Bjl-Mj.ut.AAA.0.0.Bjl-OS.AWUhAyo-6ic; c_user=100079084416483; xs=29%3AndYbm8KkI70VWg%3A2%3A1670898578%3A-1%3A7642; m_page_voice=100079084416483; wd=391x752; locale=en_US; fbl_st=100625307%3BT%3A27848309; fbl_cs=AhD%2BBtjEWp4wLlaN5IxgmLxdGGpSV2hSYUxSbUU2SXZLbWMybkl5MTNSdA; fbl_ci=841898967085320; vpd=v1%3B752x391x1.84375'
@@ -35,7 +35,6 @@ class Posts(db.Model):
 
 @app.route('/')
 def home():
-	fetch_post_and_publish()
 	acc_id=check_acc_ie(acc_ie)
 	return render_template_string(f"""
 <h2>Version: {__version__}</h2>
@@ -60,24 +59,38 @@ def write_log(msg:str):
 		log.close()
 
 def fetch_post_and_publish():
-	posts=[]
-	for page_id in page_ids:
-		scrapper=FB_Scrapper(page_id, amount_of_post_each_page, acc_ie)
-		posts+=scrapper.posts
-	for post in posts:
-		with app.app_context():
-			if not Posts.query.filter_by(content=post.content):
-				db.session.add(Posts(content=post.content))
-				db.session.commit()
-				post_fb(post.content, acc_pg)
+	try:
+		posts=[]
+		for page_id in page_ids:
+			scrapper=FB_Scrapper(page_id, amount_of_post_each_page, acc_ie)
+			posts+=scrapper.posts
+		for post in posts:
+			try:
+				with app.app_context():
+					if not Posts.query.filter_by(content=post.content).first():
+						db.session.add(Posts(content=post.content))
+						db.session.commit()
+						try:
+							post_fb(post.content, acc_pg)
+						except Exception as e:
+							write_log(f'Error at posting on facebook: {str(e)}')
+			except Exception as e:
+				write_log(f'Error at adding row to database: {str(e)}')
+				continue
+	except Exception as e:
+		write_log(f'Error at Scrapping Post: {str(e)}')
+		pass
 
 def check_delete_required():
-	with app.app_context():
-		all_posts=Posts.query.all()
-		if len(all_posts)>=10000:
-			for current_post in all_posts[:-1000]:
-				db.session.delete(current_post)
-				db.session.commit()
+	try:
+		with app.app_context():
+			all_posts=Posts.query.all()
+			if len(all_posts)>=10000:
+				for current_post in all_posts[:-1000]:
+					db.session.delete(current_post)
+					db.session.commit()
+	except Exception as e:
+		write_log(f'Error at Deleteing Post: {str(e)}')
 
 def run_schedule():
 	sleep(60)
