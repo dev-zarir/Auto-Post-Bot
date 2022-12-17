d39266951801ce6034b10502fb16f537f491ea04
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2022-12-17 22:53:55 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,164 @@
+from helper import FB_Scrapper, post_fb, check_if_valid, check_acc_ie
+from flask import Flask, render_template_string, request
+from flask_sqlalchemy import SQLAlchemy
+from time import sleep
+from threading import Thread
+
+__version__=4.1
+# EDIT THIS ++++++++++++++++++++++++++++
+amount_of_post_each_page=1
+
+page_ids=[
+	"100064620321323",
+	"100083712786515",
+	"100082319246979",
+	"100057273376837",
+	"100077463865033",
+	# "109057208214046",
+	# "110175880338880",
+	# "102260178961056",
+	"100047072903106",
+]
+
+app=Flask(__name__)
+app.config['SECRET_KEY']='uwrguyvw4buteuf4gbyugt'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://otnkpdnm:e33_gRZAfzmXc73KHUG5BWeuEX19smt2@satao.db.elephantsql.com/otnkpdnm'
+
+db=SQLAlchemy(app)
+
+class Posts(db.Model):
+	id=db.Column(db.Integer, primary_key=True)
+	content=db.Column(db.String(1000), unique=True)
+
+class Credentials(db.Model):
+	id=db.Column(db.Integer, primary_key=True)
+	token=db.Column(db.String(1000))
+	cookie=db.Column(db.String(1000))
+
+@app.route('/')
+def home():
+	is_valid=check_if_valid(get_token())
+	acc_id=check_acc_ie(get_cookie())
+	return render_template_string(f"""
+<h2>Version: {__version__}</h2>
+<h2>Token Valid: {is_valid}</h2>
+<h2>Acc Valid: {acc_id=='100075924800901'}</h2>
+<h2>Acc Checked ID: {acc_id}</h2>
+<h3><a href='/log'>View Log</a></h3>
+""")
+
+@app.route('/update', methods=['GET', 'POST'])
+def update_info():
+	if request.method=='POST':
+		idb=Credentials.query.get(1)
+		token=request.form.get('token')
+		cookie=request.form.get('cookie')
+		if token!='':
+			if not check_if_valid(token):
+				return 'Invalid Token'
+		if cookie!='':
+			if check_acc_ie(cookie)!='100075924800901':
+				return 'Invalid Cookie'
+		if token and cookie:
+			idb.token=token
+			idb.cookie=cookie
+			db.session.commit()
+			return 'Successfully Updated Token and Cookie'
+		elif token:
+			idb.token=token
+			db.session.commit()
+			return 'Successfully Updated Token'
+		elif cookie:
+			idb.cookie=cookie
+			db.session.commit()
+			return 'Successfully Updated Cookie'
+		else:
+			return 'Nothing Changed'
+	else:
+		return render_template_string("""
+<form method="POST">
+	<label for="token">Your Access Token:</label><br>
+	<textarea name="token" id="token"></textarea><br>
+	<label for="cookie">Your FB Cookie:</label><br>
+	<textarea name="cookie" id="cookie"></textarea><br>
+	<input type="submit" name="submit" value="Update">
+</form>
+""")
+
+@app.route('/log')
+def show_log():
+	with open('log.txt', 'r', encoding='utf-8') as log:
+		log_text=log.read()
+		log.close()
+	return render_template_string(log_text.replace('\n','<br>'))
+
+def get_token():
+	with app.app_context():
+		return Credentials.query.get(1).token
+
+def get_cookie():
+	with app.app_context():
+		return Credentials.query.get(1).cookie
+
+def write_log(msg:str):
+	with open('log.txt', 'r', encoding='utf-8') as log:
+		old_log=log.read()
+		log.close()
+
+	with open('log.txt', 'w', encoding='utf-8') as log:
+		log.write(old_log+'\n\n'+msg)
+		log.close()
+
+def fetch_post_and_publish():
+	try:
+		posts=[]
+		for page_id in page_ids:
+			scrapper=FB_Scrapper(page_id, amount_of_post_each_page, get_cookie())
+			posts+=scrapper.posts
+		for post in posts:
+			try:
+				with app.app_context():
+					if not Posts.query.filter_by(content=post.content).first():
+						fb_resp=post_fb(post.content, get_token())
+						if not fb_resp:
+							write_log('Error at posting on facebook: The function retuned False')
+							continue
+						db.session.add(Posts(content=post.content))
+						db.session.commit()
+			except Exception as e:
+				write_log(f'Error at adding row to database: {str(e)}')
+				continue
+	except Exception as e:
+		write_log(f'Error at Scrapping Post: {str(e)}')
+		pass
+
+def check_delete_required():
+	try:
+		with app.app_context():
+			all_posts=Posts.query.all()
+			if len(all_posts)>=10000:
+				for current_post in all_posts[:-1000]:
+					db.session.delete(current_post)
+					db.session.commit()
+	except Exception as e:
+		write_log(f'Error at Deleteing Post: {str(e)}')
+
+def run_schedule():
+	sleep(60)
+	loop=0
+	while True:
+		loop+=1
+		fetch_post_and_publish()
+		if loop % 2 == 0:
+			check_delete_required()
+		sleep(60*15)
+
+t=Thread(target=run_schedule)
+t.setDaemon(True)
+t.start()
+
+if __name__ == '__main__':
+	with app.app_context():
+		db.create_all()
+	app.run(host='0.0.0.0', port=80, debug=False)
+
---
+++ helper.py
@@ -0,0 +1,101 @@
+from requests_html import HTMLSession, HTML
+from urllib.parse import quote_plus
+import requests
+import re
+
+def check_if_valid(token):
+	resp=requests.get(f'https://graph.facebook.com/v15.0/debug_token?input_token={token}&access_token={token}')
+	try:
+		return resp.json()['data']['is_valid']
+	except:
+		return False
+
+def post_fb(message, token):
+	resp=requests.post(f"https://graph.facebook.com/v15.0/113023048137080/feed?message={quote_plus(message)}&access_token={token}")
+	if resp.status_code==200:
+		return True
+	else:
+		return False
+
+class FB_Scrapper:
+	def __init__(self, page_id:str, post_amount:int, cookie:str):
+		self.page_id=page_id
+		self.post_amount=post_amount
+		self.cookie=convert_to_dict(cookie)
+		self.cursor='abcdefg'
+		self.session=HTMLSession()
+		self.posts=self.get_posts()
+
+	def get_posts(self) -> list:
+		posts=[]
+		try:
+			while True:
+				resp=self.send_request()
+				self.cursor=resp.search('cursor={}&')[0]
+				for article in resp.find('article'):
+					if len(article.find('article'))==2:
+						if not article.find('article')[1].find('img[alt]') and not article.find('article')[1].find('div[data-ft=\'{"tn":"*s"}\']', first=True).find('a'):
+							posts.append(FB_Post(article.find('article')[1]))
+					else:
+						if not FB_Post(article) in posts:
+							if not article.find('img[alt]') and not article.find('div[data-ft=\'{"tn":"*s"}\']', first=True).find('a'):
+								posts.append(FB_Post(article))
+					if len(posts)>=self.post_amount:
+						return posts
+		except Exception as e:
+			print('Error:', str(e), self.page_id)
+			return []
+
+	def send_request(self) -> HTML:
+		resp=self.session.get(f'https://mbasic.facebook.com/profile/timeline/stream/?cursor={self.cursor}&profile_id={self.page_id}', cookies=self.cookie)
+		return resp.html
+
+class FB_Post:
+	def __init__(self, obj):
+		self.attr_id=obj.attrs['id']
+		self.attr_class=' '.join(obj.attrs['class'])
+		self.author=obj.find('h3', first=True).text
+		self.content=obj.find('div[data-ft=\'{"tn":"*s"}\']', first=True).text
+
+	def __repr__(self):
+		return self.content
+
+	def __eq__(self, other):
+		return self.content==other.content
+
+def convert_to_dict(cookie:str) -> dict:
+	try:
+		fb_cookies=cookie.replace(' ','')
+		fb_cookies=fb_cookies.replace('\n','')
+		fb_cookies=fb_cookies.split(';')
+		if '' in fb_cookies:
+			fb_cookies.remove('')
+		fb_cookies_dict={}
+		for item in fb_cookies:
+			name, value=item.split('=')
+			fb_cookies_dict[name]=value
+		return fb_cookies_dict
+	except:
+		return False
+
+def check_acc_ie(cookies:str):
+	try:
+		resp=requests.get('https://mbasic.facebook.com/smshahriar.zarir.94', cookies=convert_to_dict(cookies))
+		html=resp.text
+		match=re.findall(r'profile_id=\d+', html)
+		if not match:
+			match=re.findall(r'owner_id=\d+', html)
+		if not match:
+			match=re.findall(r'confirm/\?bid=\d+', html)
+		if not match:
+			match=re.findall(r'subscribe.php\?id=\d+', html)
+		if not match:
+			match=re.findall(r'subject_id=\d+', html)
+		if not match:
+			match=re.findall(r'poke_target=\d+', html)
+		if not match:
+			return False
+		fb_id=match[0].split('=')[1]
+		return fb_id
+	except:
+		return False
